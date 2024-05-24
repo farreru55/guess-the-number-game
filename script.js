b1b3130558ee8021bada6344d0fe7df6bb4de5ff
farreru55 <farrelendra15@gmail.com>
2024-05-24 13:14:00 +0700

    Perbaikan fitur, selesai

--- script.js
+++ script.js
@@ -9,11 +9,18 @@ if (bot == 11 || bot == 0) {
 var hasil = "";
 infi: for (var i = 5; i >= 0; i++) {
   var hello = alert("Selamat datang di permainan tebak angka");
-  sel: for (var j = 0; j < 3; j++) {
-    var selnum = parseInt(
-      prompt("Pilih dari angka 1 - 10! \nkamu punya 3 kesempatan")
-    );
-    ans: for (var k = j; k < 3; k++) {
+  sel: for (var j = 3; j >= 1; j--) {
+    var chan = j;
+    if (j == 3) {
+      var selnum = parseInt(
+        prompt("Pilih dari angka 1 - 10! \nkamu punya " + chan + " kesempatan")
+      );
+    } else if (j == 2 || j == 1) {
+      var selnum = parseInt(
+        prompt("Pilih dari angka 1 - 10! \nkesempatanmu sisa " + chan)
+      );
+    }
+    ans: for (var k = j; k >= 1; k--) {
       if (selnum == bot) {
         hasil = "benar";
         alert("Selamat jawabanmu " + hasil);
@@ -32,98 +39,20 @@ infi: for (var i = 5; i >= 0; i++) {
   if (selnum == bot) {
     var con = confirm("Apakah kamu ingin bermain lagi?");
     if (con == false) {
-      alert("Terima kasih telah bermain")
+      alert("Terima kasih telah bermain");
       break infi;
+    } else {
+      window.location.reload();
     }
   } else {
     alert("Kesempatanmu telah habis \njawaban yang tepat adalah " + bot);
+    var con = confirm("Apakah kamu ingin bermain lagi?");
+    if (con == false) {
+      alert("Terima kasih telah bermain");
+      break infi;
+    } else {
+      window.location.reload();
+    }
   }
-
-  // for (var k = j; k < 2; k++) {
-  //   if (selnum < bot) {
-  //     hasil = "terlalu kecil";
-  //     prompt("Jawabanmu " + hasil);
-  //   } else if (selnum > bot) {
-  //     hasil = "terlalu besar";
-  //     prompt("Jawabanmu " + hasil);
-  //   }
-  // }
 }
-
-// else if (selnum < bot) {
-//   hasil = "terlalu kecil";
-//   for (var k = j; k < 3; k++) {
-//     if (k == 1) {
-//       k += 1;
-//       prompt("Jawaban kamu " + hasil + "\nsisa kesempatanmu 2");
-//     } else {
-//       prompt("Jawaban kamu " + hasil + "\nsisa kesempatanmu 1");
-//     }
-//   }
-// } else if (selnum > bot) {
-//   hasil = "terlalu besar";
-//   for (var l = j; l < 3; l++) {
-//     if (l == 1) {
-//         l += 1;
-//         prompt("Jawaban kamu " + hasil + "\nsisa kesempatanmu 2");
-//       } else {
-//         prompt("Jawaban kamu " + hasil + "\nsisa kesempatanmu 1");
-//       }
-//   }
-// }
-//  else {
-//   alert("WAH");
-// }
-
-//   if (selnum != bot) {
-//     alert("Yang benar jawabannya adalah " + bot);
-//   }
-//   var i = confirm("Apakah kamu ingin bermain lagi?");
-//   if (i == false) {
-//     break infi;
-//   }
-
-// var hasil = "";
-// ques: for (var j = 5; j >= 0; j--) {
-//     // Pilih angka
-//     var selnum = parseInt(prompt("Pilih dari angka 1 - 10! \nkamu punya 3 kesempatan"));
-
-//     // Rules
-//     rules: for (var i = 0; i < 3; i++) {
-//         if (selnum == bot) {
-//             hasil = "benar";
-//             alert("Selamat jawabanmu " + hasil);
-//             var i = confirm("Mau main lagi?");
-//             if(i == false) {
-//                 break ques;
-//             }
-//         } else if (selnum < bot) {
-//             hasil = "terlalu kecil";
-//             if (i == 0 || i == 1) {
-//                 alert("Jawabanmu " + hasil + "\nkesempatanmu tinggal 2")
-//             } else {
-//                 alert("Jawabanmu " + hasil + "\nkesempatanmu tinggal 1")
-//             }
-//         } else if (selnum > bot) {
-//             hasil = "terlalu besar";
-//             if (i == 0) {
-//                alert("Jawabanmu " + hasil + "\nkesempatanmu tinggal 2");
-//             } else if (i == 1) {
-//                 alert("Jawabanmu " + hasil + "\nkesempatanmu tinggal 1");
-//             } else {
-//                 alert("GAGAGA");
-//             }
-//         } else {
-//             hasil = "Pilih yang benar";
-//         }
-//     }
-
-//     // alert("Jawabanmu " + hasil);
-// }
-// if (selnum == bot) {
-//     alert("Terima kasih telah bermain");
-// } else {
-//     alert("Silahkan coba lagi \njawaban yang tepat adalah " + bot);
-// }
-
 // console.log(bot);

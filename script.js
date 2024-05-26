736c6a76c05a3aebefb885de876e74c092e671c0
farreru55 <farrelendra15@gmail.com>
2024-05-26 21:18:34 +0700

    Change language to English

--- script.js
+++ script.js
@@ -8,47 +8,47 @@ if (bot == 11 || bot == 0) {
 
 var hasil = "";
 infi: for (var i = 5; i >= 0; i++) {
-  var hello = alert("Selamat datang di permainan tebak angka");
+  var hello = alert("Welcome to guess the number game!");
   sel: for (var j = 3; j >= 1; j--) {
     var chan = j;
     if (j == 3) {
       var selnum = parseInt(
-        prompt("Pilih dari angka 1 - 10! \nkamu punya " + chan + " kesempatan")
+        prompt("Choose number between 1 and 10 \nyou have " + chan + " chances")
       );
     } else if (j == 2 || j == 1) {
       var selnum = parseInt(
-        prompt("Pilih dari angka 1 - 10! \nkesempatanmu sisa " + chan)
+        prompt("Choose number between 1 and 10 \nyou have " + chan  + " chances left")
       );
     }
     ans: for (var k = j; k >= 1; k--) {
       if (selnum == bot) {
-        hasil = "benar";
-        alert("Selamat jawabanmu " + hasil);
+        result = "correct";
+        alert("Congratulations! your answer is " + result);
         break sel;
       } else if (selnum < bot) {
-        hasil = "terlalu kecil";
-        alert("Jawabanmu " + hasil);
+        result = "too small";
+        alert("your answer is " + result);
         break ans;
       } else if (selnum > bot) {
-        hasil = "terlalu besar";
-        alert("Jawabanmu " + hasil);
+        result = "too big";
+        alert("your answer is " + result);
         break ans;
       }
     }
   }
   if (selnum == bot) {
-    var con = confirm("Apakah kamu ingin bermain lagi?");
+    var con = confirm("Do you want to play again?");
     if (con == false) {
-      alert("Terima kasih telah bermain");
+      alert("Thank you for playing");
       break infi;
     } else {
       window.location.reload();
     }
   } else {
-    alert("Kesempatanmu telah habis \njawaban yang tepat adalah " + bot);
-    var con = confirm("Apakah kamu ingin bermain lagi?");
+    alert("Your chance is over \nthe correct answer is " + bot);
+    var con = confirm("Do you want to play again?");
     if (con == false) {
-      alert("Terima kasih telah bermain");
+      alert("Thank you for playing");
       break infi;
     } else {
       window.location.reload();
